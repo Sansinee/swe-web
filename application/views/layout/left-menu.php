6c4e2a442949cc7bfb1657132ff28d73455b4a46
Sansinee <gift.chill.chill@gmail.com>
2020-04-29 18:56:52 +0700

    add

--- application/views/layout/left-menu.php
+++ application/views/layout/left-menu.php
@@ -1,49 +1,38 @@
-<section class="htc__product__grid bg__white ptb--100">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-9 col-lg-push-3 col-md-9 col-md-push-3 col-sm-12 col-xs-12">
-                        <div class="htc__product__rightidebar">
-                            <div class="htc__grid__top">
-                                
-                                <div class="ht__pro__qun">
-                                    <span>Showing 1-12 of 1033 products</span>
-                                </div>
-                                <!-- Start List And Grid View -->
-                                <ul class="view__mode" role="tablist">
-                                    <li role="presentation" class="grid-view active"><a href="#grid-view" role="tab" data-toggle="tab"><i class="zmdi zmdi-grid"></i></a></li>
-                                    <li role="presentation" class="list-view"><a href="#list-view" role="tab" data-toggle="tab"><i class="zmdi zmdi-view-list"></i></a></li>
-                                </ul>
-                                <!-- End List And Grid View -->
+<div class="col-lg-3 col-lg-pull-9 col-md-3 col-md-pull-9 col-sm-12 col-xs-12 smt-40 xmt-40">
+                <div class="htc__product__leftsidebar">
+                    <!-- Start Prize Range -->
+                    <div class="htc-grid-range">
+                        <h4 class="title__line--4">Price</h4>
+                        <div class="content-shopby">
+                            <div class="price_filter s-filter clear">
+                                <form action="#" method="GET">
+                                    <div id="slider-range"></div>
+                                    <div class="slider__range--output">
+                                        <div class="price__output--wrap">
+                                            <div class="price--output">
+                                                <span>Price :</span><input type="text" id="amount" readonly>
+                                            </div>
+                                            <div class="price--filter">
+                                                <a href="#">Filter</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
-                           
-                        </div>
-                       
-                    </div>
-                    <div class="col-lg-3 col-lg-pull-9 col-md-3 col-md-pull-9 col-sm-12 col-xs-12 smt-40 xmt-40">
-                        <div class="htc__product__leftsidebar">
-                            
-                            <!-- Start Category Area -->
-                            <div class="htc__category">
-                                <h4 class="title__line--4">categories</h4>
-                                <ul class="ht__cat__list">
-                                    <li><a href="#">Clothing</a></li>
-                                    <li><a href="#">Bags</a></li>
-                                    <li><a href="#">Shoes</a></li>
-                                    <li><a href="#">Jewelry</a></li>
-                                    <li><a href="#">Accessories</a></li>
-                                    <li><a href="#">Food / Drink Store</a></li>
-                                    <li><a href="#">Gift Store</a></li>
-                                    <li><a href="#">Accessories</a></li>
-                                    <li><a href="#">Watch</a></li>
-                                    <li><a href="#">Other</a></li>
-                                </ul>
-                            </div>
-                            <!-- End Category Area -->
-                                                                                
-                
-                         
                         </div>
                     </div>
+                    <!-- End Prize Range -->
+                    <!-- Start Category Area -->
+                    <div class="htc__category">
+                        <h4 class="title__line--4">categories</h4>
+                        <ul class="ht__cat__list">
+                            <?php foreach ($categories as $row) { ?>
+                                <li><a href="<?php echo base_url('categorise/'.$row['_id']); ?>"><?php echo $row['name'] ?></a></li>
+                            <?php } ?>
+                        </ul>
+                    </div>
+                    <!-- End Category Area -->
+                    
                 </div>
             </div>
-        </section>
+        </div>
